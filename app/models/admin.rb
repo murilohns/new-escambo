39ce2fe1dde8b8762f684a545fb2fa1373097011
Murilo Henrique <murilohns@gmail.com>
2017-04-04 19:20:06 +0000

    Fixed admin.rb

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,13 @@
 class Admin < ApplicationRecord
-  enum role: {:full_access => 0, :restricted_access => 1}
+  #Constants
+  ROLES = {:full_access => 0, :restricted_access => 1}
 
-  scope :with_full_access, -> { where(role: 'full_access') }
+  #Enums
+  enum role: ROLES
+
+  #Scopes
+  scope :with_full_access, -> { where(role: ROLES[:full_access]) }
+  scope :with_restricted_access, -> { where(role: ROLES[:restricted_access]) }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
