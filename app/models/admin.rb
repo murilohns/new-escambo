5106c21657a92672ff03718b9235627342018c22
Murilo Henrique <murilohns@gmail.com>
2017-04-04 13:59:44 +0000

    Added Scope 'admin with full access'

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,7 @@
 class Admin < ApplicationRecord
   enum role: [:full_access, :restricted_access]
+
+  scope :with_full_access, -> { where(role: 'full_access') }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
