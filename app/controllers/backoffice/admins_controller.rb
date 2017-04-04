ec794d99fcd7452f72b614512aec4daee4740df6
Murilo Henrique <murilohns@gmail.com>
2017-04-04 20:04:24 +0000

    Fixed admins controller

--- app/controllers/backoffice/admins_controller.rb
+++ app/controllers/backoffice/admins_controller.rb
@@ -23,14 +23,6 @@ class Backoffice::AdminsController < BackofficeController
   end
 
   def update
-    passwd = params[:admin][:password]
-    passwd_confirmation = params[:admin][:password_confirmation]
-
-    if passwd.blank? && passwd_confirmation.blank?
-      params[:admin].delete(:password)
-      params[:admin].delete(:password_confirmation)
-    end
-
     if @admin.update(params_admin)
       redirect_to backoffice_admins_path, notice: "O Administrador #{@admin.email} foi atualizado com sucesso"
     else
@@ -54,6 +46,13 @@ class Backoffice::AdminsController < BackofficeController
   end
   
   def params_admin
+    passwd = params[:admin][:password]
+    passwd_confirmation = params[:admin][:password_confirmation]
+
+    if passwd.blank? && passwd_confirmation.blank?
+      params[:admin].except!(:password, :password_confirmation)
+    end
+
      params.require(:admin).permit(:email, :password, :password_confirmation, :name)
   end 
   
